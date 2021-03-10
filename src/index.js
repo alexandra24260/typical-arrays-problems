efa78536e78e657d33d8b266b64aee8d83d172b0
alexandra.Vecher <alexandra_vecher@epam.com>
2021-03-10 18:25:03 +0300

    feat: solution for typical array problem

--- src/index.js
+++ src/index.js
@@ -1,12 +1,34 @@
-
 exports.min = function min (array) {
-  return 0;
+  if (array && array.length){
+    $min = Math.min.apply(null, array);
+    return $min;
+  }
+  else {
+    return 0;
+  }
 }
 
 exports.max = function max (array) {
+if (array && array.length){
+  $max = Math.max.apply(null, array);
+  return $max;
+}
+else {
   return 0;
+}  
 }
 
 exports.avg = function avg (array) {
+if (array && array.length){
+  let result = 0;
+    for (let i = 0; i < array.length; i++) {
+      result += array[i];
+    }
+  let average = result/array.length;
+  return average;
+}
+else {
   return 0;
 }
+}
+
